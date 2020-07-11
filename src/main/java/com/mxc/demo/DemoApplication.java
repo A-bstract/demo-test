80d186367ab9bf6f04af7716909408c6c367c717
A_bstract <15710563453@163.com>
2020-07-11 10:34:24 +0800

    111

--- src/main/java/com/mxc/demo/DemoApplication.java
+++ src/main/java/com/mxc/demo/DemoApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    //启动很累
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+    //启动不累
 
 }
