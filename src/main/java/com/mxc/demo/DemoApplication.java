10cc9f4bc763f50061ab0c265d5b6c78f0800012
A_bstract <15710563453@163.com>
2020-07-13 16:01:25 +0800

    不想提交到远程分支

--- src/main/java/com/mxc/demo/DemoApplication.java
+++ src/main/java/com/mxc/demo/DemoApplication.java
@@ -10,6 +10,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-    //启动不累 -1
+    //启动不累 -1 -0
 
 }
